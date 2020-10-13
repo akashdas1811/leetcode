18267ea9c0ddb8177f4b82e6ebf701a6fba898b3
Akash Das <50514652+akashdas1811@users.noreply.github.com>
2020-10-13 21:42:48 +0530

    Add files via upload

---
+++ Sort_linked_list.cpp
@@ -0,0 +1,80 @@
+/*Given the head of a linked list, return the list after sorting it in ascending order.
+
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+
+ 
+
+Example 1:
+
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+
+Example 2:
+
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+
+Example 3:
+
+Input: head = []
+Output: []
+
+ 
+
+Constraints:
+
+    The number of nodes in the list is in the range [0, 5 * 104].
+    -105 <= Node.val <= 105
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *merge(ListNode *a,ListNode *b)
+    {
+        ListNode *ans=NULL;
+        if(a==NULL)
+            return b;
+        if(b==NULL)
+            return a;
+        if(a->val<=b->val)
+        {
+            ans=a;
+            a->next=merge(a->next,b);
+        }
+        else
+        { 
+            ans=b;
+            b->next=merge(a,b->next);
+        }
+        return ans;
+    }
+    ListNode* sortList(ListNode* head) {
+        if(head==NULL || head->next==NULL)
+            return head;
+        ListNode *slow=head;
+        ListNode *fast=head;
+        ListNode *prev=NULL;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            prev=slow;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        prev->next=NULL;
+
+        ListNode *l1=sortList(slow);
+        ListNode *l2=sortList(head);
+
+        return merge(l1,l2);
+    }
+};
